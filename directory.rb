44e682ce5eb8545a341b4c8f476f0e3340f4aac1
Matt Firmston <mattfirmston@gmail.com>
2021-06-16 10:34:39 +0100

    centered names on print, created input with default values for cohort

--- directory.rb
+++ directory.rb
@@ -1,20 +1,15 @@
-# gets input from the user to use as student names, and returns an array of hashes
 def input_students
   puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # create and empty array
+  puts "To finish, leave the name blank and hit return again"
   students = []
-  # get the first name
   name = gets.chomp
-  # while the name is not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
+    puts "What chort does this student belong to?"
+    cohort = gets.chomp
+    students << {name: name, cohort: cohort.empty? ? "permanent" : cohort }
     puts "Now we have #{students.count} students"
-    # get another name from the user
     name = gets.chomp
   end
-  # return the array of students
   students
 end
 
@@ -25,7 +20,7 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{index + 1}. #{student[:name].center(30)} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -54,5 +49,5 @@ end
 # nothing happens until we call the methods
 students = input_students
 print_header
-print_shorter_than_twelve(students)
+print(students)
 print_footer(students)
