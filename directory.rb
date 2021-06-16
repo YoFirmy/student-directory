c96d9b3c9bbf92ecbf17c25193de4f8a46d12000
Matt Firmston <mattfirmston@gmail.com>
2021-06-16 14:46:39 +0100

    enabled saving data to file

--- directory.rb
+++ directory.rb
@@ -17,6 +17,16 @@ def input_students
   end
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -29,24 +39,6 @@ def print_students
   end
 end
 
-def print_beginning_with(letter)
-  puts "students beginning with #{letter}:"
-  @students.each do |student|
-    if student[:name][0].upcase == letter
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end
-  end
-end
-
-def print_shorter_than_twelve
-  puts "students with names shorter than 12 characters long:"
-  @students.each do |student|
-    if student[:name].length < 12
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end
-  end
-end
-
 def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
@@ -54,6 +46,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -69,6 +62,8 @@ def process(selection)
     students = input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
