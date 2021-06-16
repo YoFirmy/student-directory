6df467b46067779b490c1a5c3ed7e4f896223a84
Matt Firmston <mattfirmston@gmail.com>
2021-06-16 15:24:50 +0100

    enabled loading students to list from file

--- directory.rb
+++ directory.rb
@@ -27,6 +27,15 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, chort.to_sym}  
+  end
+  file.close
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -47,6 +56,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -64,6 +74,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
